1620fd3e46dd78ef43f740ebc925c4ec75f2edc6
Benjamin Loison <benjamin.loison@orange.fr>
2019-06-13 20:04:18 +0200

    Create biome.h

---
+++ Client/LemnosLife/Map/biome.h
@@ -0,0 +1,14 @@
+#ifndef DEF_BIOME
+#define DEF_BIOME
+
+#define BIOMES_NUMBER 19
+#define BIOME_DEFAULT_ASCII_INDEX 54
+#include <string>
+
+void loadBiomeChk1k(std::string, bool = false);
+char getBiome(double[2]), getBiome(double, double, std::string);
+std::string getBiomeName(char), getBiomeName(double, double), getBiomeName(double, double, std::string);
+
+extern std::string biomesNames[BIOMES_NUMBER]; 
+
+#endif
