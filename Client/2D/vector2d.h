46e865a018c48c56fcdfe8b6e29ac3e0094d0393
Benjamin Loison <benjamin.loison@orange.fr>
2019-06-16 23:31:20 +0200

    Create vector2d.h

---
+++ Client/2D/vector2d.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+#include "../3D/Render/Engine/vector3d.h"
+#include <vector>
+
+class Vector2D
+{
+    public:
+        double X, Y, length() const;
+        Vector2D(double = 0, double = 0);
+        Vector2D(std::vector<double>);
+        Vector2D(const double coordinates[2]);
+        Vector2D(Vector2D, Vector2D);
+        Vector2D(Vector3D);
+};
+
+double determinant(Vector2D, Vector2D), determinant(Vector2D, Vector2D, Vector2D);
+
+#endif
