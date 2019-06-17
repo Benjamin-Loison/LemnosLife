adcfa130e82d3892ffa3337d7fa5d73e60038c2f
Benjamin Loison <benjamin.loison@orange.fr>
2019-06-17 19:11:37 +0200

    Update and rename Installalation/Updater/patch.cpp to Installation/Updater/patch.cpp

--- Installalation/Updater/patch.cpp
+++
@@ -1,4 +0,0 @@
-#include <string>
-using namespace std;
-
-string name = "AltisCraft.fr - Système de mises à jour (Mise à jour en cours)";
---
+++ Installation/Updater/patch.cpp
@@ -0,0 +1,4 @@
+#include <string>
+using namespace std;
+
+string name = "LemnosLife - Système de mises à jour (Mise à jour en cours)";
