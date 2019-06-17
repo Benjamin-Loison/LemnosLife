4be79bfc62bf0df024a756d3783ce142f4af44a6
Benjamin Loison <benjamin.loison@orange.fr>
2019-06-17 19:09:10 +0200

    Coded on the 03/07/17

---
+++ Installalation/Updater/patch.cpp
@@ -0,0 +1,4 @@
+#include <string>
+using namespace std;
+
+string name = "AltisCraft.fr - Système de mises à jour (Mise à jour en cours)";
---
+++ Installalation/Updater/patch.h
@@ -0,0 +1,7 @@
+#ifndef PATCH_H_INCLUDED
+#define PATCH_H_INCLUDED
+#include <string>
+
+extern std::string name;
+
+#endif
