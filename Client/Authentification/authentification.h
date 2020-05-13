f90c75ecca96d349a4e2e1d07d317a9da5fb8e51
Benjamin Loison <benjamin.loison@orange.fr>
2020-05-13 17:31:13 +0200

    Add files via upload

---
+++ Client/Authentification/authentification.h
@@ -0,0 +1,8 @@
+#ifndef AUTHENTIFICATION_H_INCLUDED
+#define AUTHENTIFICATION_H_INCLUDED
+
+#include <string>
+
+std::string hashPassword(std::string), createKey(std::string);
+
+#endif
---
+++ Client/Authentification/bcrypt.h
@@ -0,0 +1,9 @@
+#ifndef BCRYPT_H_INCLUDED
+#define BCRYPT_H_INCLUDED
+
+#include <string>
+
+int bcrypt_newhash(const char*, int, char*, size_t);
+std::string bcrypt_checkpass(std::string/*const char**/, const char*);
+
+#endif
