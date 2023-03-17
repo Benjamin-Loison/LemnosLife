712621b40532296c5017960d4f16ed842799162b
Benjamin Loison <Benjamin_Loison@users.noreply.gitea.lemnoslife.com>
2023-03-17 04:32:50 +0100

    Remove `Installation/Updater/patch.{cpp, h}`

--- Installation/Updater/patch.cpp
+++
@@ -1,4 +0,0 @@
-#include <string>
-using namespace std;
-
-string name = "LemnosLife - Système de mises à jour (Mise à jour en cours)";
--- Installation/Updater/patch.h
+++
@@ -1,7 +0,0 @@
-#ifndef PATCH_H_INCLUDED
-#define PATCH_H_INCLUDED
-#include <string>
-
-extern std::string name;
-
-#endif
