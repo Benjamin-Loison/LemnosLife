b64b73c1ff42f10194cd5d34836ed160927706d9
Benjamin Loison <12752145+Benjamin-Loison@users.noreply.github.com>
2023-03-27 02:53:44 +0200

    Remove unused code from shortcut

--- Installation/Shortcut/main.cpp
+++ Installation/Shortcut/main.cpp
@@ -1,35 +1,10 @@
 #include <string>
-#include <fstream>
 #include "shlobj.h"
 using namespace std;
 
 /// TODO: make a graphical message if the destination executable path isn't reachable
-int main() /// TODO: remove useless console(installer works well without it) come from project settings I think (initial) if needed try to recreate a EMPTY PROJECT
+int main()
 {
-    /*int hConHandle;
-    long lStdHandle;
-    CONSOLE_SCREEN_BUFFER_INFO coninfo;
-    FILE *fp;
-    AllocConsole();
-    SetConsoleTitle("");
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
-    lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen(hConHandle, "w");
-    *stdout = *fp;
-    setvbuf(stdout, NULL, _IONBF, 0);
-    lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen(hConHandle, "r");
-    *stdin = *fp;
-    setvbuf(stdin, NULL, _IONBF, 0);
-    lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-    fp = _fdopen(hConHandle, "w");
-    *stderr = *fp;
-    setvbuf(stderr, NULL, _IONBF, 0);
-    ios::sync_with_stdio();*/
     /*string name = "LemnosLife - Installeur.exe";
     ifstream installer(name.c_str());
     if(installer.good())
@@ -43,8 +18,6 @@ int main() /// TODO: remove useless console(installer works well without it) com
     #else
         '/';
     #endif
-    //TCHAR szPath[MAX_PATH];
-    //SHGetFolderPath(NULL, CSIDL_COMMON_APPDATA, NULL, 0, szPath);
     string tmp = string(getenv("APPDATA")) + pathSeparator + "TerraCraft" + pathSeparator + "Games" + pathSeparator + "LemnosLife" + pathSeparator + "Updater" + pathSeparator,
            path = tmp + "LemnosLife - MAJ.exe";
     STARTUPINFO si;
