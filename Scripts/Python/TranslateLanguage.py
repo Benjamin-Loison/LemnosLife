fd842f65359dabb2fed78c9e07703e9d26818ebf
Benjamin Loison <benjamin_loison@users.noreply.gitea.lemnoslife.com>
2023-07-28 11:44:35 +0200

    Clean `Scripts/Python/TranslateLanguage.py`

--- Scripts/Python/TranslateLanguage.py
+++ Scripts/Python/TranslateLanguage.py
@@ -8,34 +8,28 @@ os.chdir(path)
 translator = Translator()
 
 def lg(key):
-    return translator.translate(key, dest='de', src="fr").text
+    return translator.translate(key, dest='de', src='fr').text
 
-f = open("FR_fr.lang")
-lines = f.readlines()
-f.close()
+with open('FR_fr.lang') as f:
+    lines = f.readlines()
 
-f = open("DE_de.lang", 'w')
+with open('DE_de.lang', 'w') as f:
+    linesLen = len(lines)
+    for linesIndex, line in enumerate(lines):
+        newLine = line
+        lineParts = line.split('=')
+        linePartsLen = len(lineParts)
+        if linePartsLen >= 2:
+            translated = lg(lineParts[1])
+            newLine = lineParts[0] + '=' + translated
+        f.write(newLine)
+        print(newLine)
+        if linesIndex < linesLen - 1:
+            f.write("\n")
 
-linesLen = len(lines)
-
-for linesIndex in range(linesLen):
-    line = lines[linesIndex]
-    newLine = line
-    lineParts = line.split('=')
-    linePartsLen = len(lineParts)
-    if linePartsLen >= 2:
-        translated = lg(lineParts[1])
-        newLine = lineParts[0] + '=' + translated
-    f.write(newLine)
-    print(newLine)
-    if linesIndex < linesLen - 1:
-        f.write("\n")
-
-# should translate even if <>
-# what do with //comments ?
-# how to deal with /commands ?
-# should make if translated and lineParts[0] are the same, set as a comment
-# how to deal is punktuation ? (not well managed with google translator) and ": " at the end ?
-# one more return line after comments, why ?
-
-f.close()
+    # should translate even if <>
+    # what do with //comments ?
+    # how to deal with /commands ?
+    # should make if translated and lineParts[0] are the same, set as a comment
+    # how to deal is punktuation ? (not well managed with google translator) and ": " at the end ?
+    # one more return line after comments, why ?
