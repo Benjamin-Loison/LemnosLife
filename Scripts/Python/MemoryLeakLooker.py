2eefaad63bb67d9fa5975c423556b4b2dc625abe
Benjamin Loison <benjamin_loison@users.noreply.gitea.lemnoslife.com>
2023-07-28 11:19:25 +0200

    Clean `Scripts/Python/MemoryLeakLooker.py`

--- Scripts/Python/MemoryLeakLooker.py
+++ Scripts/Python/MemoryLeakLooker.py
@@ -4,24 +4,25 @@
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
+import shlex
 
 def getRAMUsage(software):
-    res = subprocess.check_output(["tasklist", "|", "findstr", software], shell=True)
+    res = subprocess.check_output(f'tasklist | findstr {shlex.quote(software)}', shell = True)
     parts = res.split()
-    parts[-2] = float(parts[-2].decode("utf-8").replace(',', '.'))
-    sizeType = parts[-1].decode("utf-8")
+    parts[-2] = float(parts[-2].decode('utf-8').replace(',', '.'))
+    sizeType = parts[-1].decode('utf-8')
     if sizeType == 'K':
         parts[-2] *= 1000
     elif sizeType == 'M':
         parts[-2] *= 1000000
     return parts[-2]
 
-#print(getRAMUsage("LemnosLife"))
+#print(getRAMUsage('LemnosLife'))
 
 i = 0
 
 while True:
-    y = getRAMUsage("LemnosLife")
+    y = getRAMUsage('LemnosLife')
     plt.scatter(i, y)
     plt.pause(0.05)
     i += 1
